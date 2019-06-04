823c34f61ce60fa7b54503e1ec302dea5f9deff2
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-06-04 11:46:18 -0500

    Fixed up the ridge regression code a bit.

--- code/ridge.R
+++ code/ridge.R
@@ -1,7 +1,8 @@
-# TO DO: Explain here what this function does, and how to use it.
+# Fit a ridge regression model by running a fixed number of
+# co-ordinate ascent updates.
 ridge <- function (X, y, b0, s0, numiter = 100) {
 
-  # Get the initial estimate of the solution.
+  # Get the initial estimate.
   b <- b0
   
   # This variable is used to keep track of the algorithm's progress;
@@ -23,15 +24,13 @@ ridge <- function (X, y, b0, s0, numiter = 100) {
   return(list(b = b,value = value))
 }
 
-# TO DO: Explain here what this function does, and how to use it.
+# Fit a ridge regression model by running a fixed number of
+# co-ordinate ascent updates accelerated using the DAAREM method.
 daarridge <- function (X, y, b0, s0, numiter = 100, order = 10) {
 
-  # Get the initial estimate of the solution.
-  b <- b0
-  
   # Run DAAREM.
   out <- suppressWarnings(
-    daarem(b,daarridge.update,daarridge.objective,X,y,s0,
+    daarem(b0,daarridge.update,daarridge.objective,X,y,s0,
            control = list(maxiter = numiter,order = order,tol = 0,
                           mon.tol = 0,kappa = 20,alpha = 1.2)))
 
@@ -40,25 +39,25 @@ daarridge <- function (X, y, b0, s0, numiter = 100, order = 10) {
   return(list(b = out$par,value = out$objfn.track[-1]))
 }
 
-# Compute the value of the log-posterior (i.e., the log-likelihood
-# with the ridge "penalty term") up to a constant of proportionality.
-ridge.objective <- function (X, y, b, s0)
-  -(norm2(y - X %*% b)^2 + norm2(b/s0)^2)
-
-# Run each of the co-ordinate ascent updates once for each co-ordinate.
-ridge.update <- function (X, y, b, s0) {
-  p <- length(b)
-  xy <- drop(y %*% X)
-  R  <- crossprod(X)
-  for (i in 1:p)
-    b[i] <- (xy[i] - sum(R[i,-i] * b[-i]))/(R[i,i] + 1/s0^2)
-  return(b)
-}
-
 # This implements the objfn argument for the daarem call above.
 daarridge.objective <- function (b, X, y, s0)
   ridge.objective(X,y,b,s0)
 
 # This implements the fixptfn argument for the daarem call above.
 daarridge.update <- function (b, X, y, x0)
-  ridge.update(X,y,b,s0)
+    ridge.update(X,y,b,s0)
+
+# Compute the value of the log-posterior (i.e., the log-likelihood
+# with the ridge "penalty term") up to a proportionality constant.
+ridge.objective <- function (X, y, b, s0)
+  -(norm2(y - X %*% b)^2 + norm2(b/s0)^2)
+
+# Apply a co-ordinate ascent update once for each co-ordinate.
+ridge.update <- function (X, y, b, s0) {
+  p  <- length(b)
+  xy <- drop(y %*% X)
+  R  <- crossprod(X)
+  for (i in 1:p)
+    b[i] <- (xy[i] - sum(R[i,-i] * b[-i]))/(R[i,i] + 1/s0^2)
+  return(b)
+}
