121920f96ad4286660c3d149e42e7f9a3840b1c8
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-10-29 15:29:31 -0500

    Implemented cost function for NMF.

--- code/nmf.R
+++ code/nmf.R
@@ -6,6 +6,7 @@ betanmf <- function (X, A, B, numiter = 100, e = 1e-15) {
   value <- rep(0,numiter)
   
   # Iterate the multiplicative (EM) updates.
+  cat("iter objective (cost fn)\n")
   for (i in 1:numiter) {
 
     # Update the loadings ("activations").
@@ -17,9 +18,8 @@ betanmf <- function (X, A, B, numiter = 100, e = 1e-15) {
     B <- pmax(B,e)
 
     # Record the algorithm's progress.
-    AB       <- A %*% B
-    value[i] <- sum(AB - X*log(AB + e))
-
+    value[i] <- cost(X,A,B,e)
+    cat(sprintf("%4d %+0.12e\n",i,value[i]))
   }
 
   # Return the estimated factors and loadings, and the value of the
@@ -27,6 +27,39 @@ betanmf <- function (X, A, B, numiter = 100, e = 1e-15) {
   return(list(A = A,B = B,value = value))
 }
 
+# TO DO: Explain here what this function does, and how to use it.
+daarbetanmf <- function (X, A, B, numiter = 100,order = 10,e = 1e-15) {
+
+  # Run DAAREM.
+  out <- suppressWarnings(
+    daarem(x0,daarbetanmf.update,daarbetanmf.objective,X,e,
+           control = list(maxiter = numiter,order = order,tol = 0,
+                          mon.tol = 0.05,kappa = 20,alpha = 1.2)))
+
+  # Return the estimate of the solution and the value of the objective
+  # at each iteration.
+  # TO DO.
+}
+
+# This implements the fixptfn argument for the daarem call above.
+daarbetanmf.update <- function (vars, X, e) {
+  # TO DO.
+}
+
+# This implements the objfn argument for the daarem call above.
+daarbetanmf.objective <- function (vars, X, e) {
+  # TO DO.
+}
+
 # Scale each column A[,i] by b[i].
 scale.cols <- function (A, b) 
   t(t(A) * b)
+
+# Compute the value of the cost function for non-negative matrix
+# factorization, in which matrix X is approximated by matrix AB = A*B.
+# This is equivalent to the negative Poisson log-likelihood after
+# removing terms that do not depend on A or B.
+cost <- function (X, A, B, e) {
+  AB <- A %*% B
+  return(sum(AB - X*log(AB + e)))
+}
